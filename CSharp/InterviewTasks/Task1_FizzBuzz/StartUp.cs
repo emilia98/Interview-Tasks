fb3d978ff419d08b5c082253a0162184f54893f4
emilia98 <emilia_nedialkova@abv.bg>
2020-11-14 01:13:23 +0200

    add C# Solutions

---
+++ CSharp/InterviewTasks/Task1_FizzBuzz/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1_FizzBuzz
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string result = String.Empty;
+
+                if (i % 3 == 0)
+                {
+                    result += "Fizz";
+
+                    if (i % 5 == 0)
+                    {
+                        result += " Buzz";
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    result += "Buzz";
+                }
+                else
+                {
+                    result = i.ToString();
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ CSharp/InterviewTasks/Task2_HappyYear/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2_HappyYear
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Greet(2018);
+
+            Greet(2019);
+
+            Greet(9999);
+        }
+
+        private static void Greet(int givenYear)
+        {
+            long year = givenYear;
+            bool isHappyYear = false;
+
+            while (!isHappyYear)
+            {
+                year++;
+                isHappyYear = FindIfYearIsHappy(year);
+            }
+
+            if (isHappyYear)
+            {
+                Console.WriteLine($"Good Bye, {givenYear}! See you in {year}!");
+            }
+        }
+
+        private static bool FindIfYearIsHappy(long year)
+        {
+            long temp = year;
+            HashSet<int> digits = new HashSet<int>();
+
+            while (temp > 0)
+            {
+                int lastDigit = (int)(temp % 10);
+                temp /= 10;
+
+                if (digits.Contains(lastDigit))
+                {
+                    return false;
+                } 
+
+                digits.Add(lastDigit);
+            }
+
+            return true;
+        }
+    }
+}
